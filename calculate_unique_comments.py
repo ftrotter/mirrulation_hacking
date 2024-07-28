7f2424732c28b9adf3c8fe27b4821b8e3dc30e62
Fred Trotter <fred.trotter@gmail.com>
2024-07-28 17:49:47 -0400

    seperate this out

---
+++ calculate_unique_comments.py
@@ -0,0 +1,68 @@
+from trottertools.WriteOnceDict import WriteOnceDict
+from trottertools.myDBTable import myDBTable
+from trottertools.mySQLh import mySQLh
+import os
+import json
+import sys
+import sqlalchemy as db
+import re
+import difflib
+from difflib import unified_diff
+from sqlalchemy import select
+from sqlalchemy import text
+import MySQLdb
+from difflib import HtmlDiff
+import argparse
+
+
+if __name__ == "__main__":
+
+    
+    comment_table = 'comment'
+    m_db = 'mirrulation'
+    commentDBT = myDBTable.myDBTable(m_db, comment_table)
+
+    unique_comment_DBT = myDBTable.myDBTable(m_db,'uniquecomment')
+    cluster_DBT = myDBTable.myDBTable(m_db,'comment_cluster')
+    unique_to_comment_DBT = myDBTable.myDBTable(m_db,'uniquecomment_to_comment')
+
+#    if len(sys.argv) < 2:
+#        print("Usage: python import_json.py <directory_path>")
+#        sys.exit(1)
+
+
+    sql_dict = WriteOnceDict.WriteOnceDict()
+
+    sql_dict['drop unique table'] = f"DROP TABLE IF EXISTS {unique_comment_DBT}"
+
+    sql_dict['create unique table']  =  f"""
+CREATE TABLE {unique_comment_DBT}
+SELECT 
+    simplified_comment_text,
+    COUNT(DISTINCT(commentId)) AS comment_count
+FROM {commentDBT}
+GROUP BY simplified_comment_text
+ORDER BY comment_count DESC
+""" 
+
+    sql_dict['add an id as the prrimary key'] = f"ALTER TABLE {unique_comment_DBT}  ADD `id` INT(11) NOT NULL AUTO_INCREMENT  FIRST,  ADD   PRIMARY KEY  (`id`);"    
+
+    sql_dict['drop the unique to comment map'] = f"DROP TABLE IF EXISTS {unique_to_comment_DBT}"
+
+    sql_dict['create the unique to comment map'] = f"""
+CREATE TABLE {unique_to_comment_DBT} AS 
+SELECT 
+    acomment.id AS comment_id,
+    uniquecomment.id AS uniquecomment_id
+
+FROM  {unique_comment_DBT} AS uniquecomment 
+JOIN {commentDBT} AS acomment ON 
+    acomment.simplified_comment_text = 
+    uniquecomment.simplified_comment_text
+ORDER BY acomment.id, uniquecomment.id
+"""
+
+    SQLh = mySQLh.mySQLh()
+
+    is_just_print = False
+    SQLh.runQuerys(sql_dict,is_just_print)
